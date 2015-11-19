5f960b2b43407bcb026d6ec10b53ac872e585c16
Davide Berti <davide99.db@libero.it>
2015-11-19 19:00:29 +0100

    Esercizio n°49 - Potenza

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+    {
+   int x, y;
+   int cont;
+        cont=0;
+        cout<<"Scrivi il numero intero da moltiplicare"<<endl;
+        cin>>x;
+
+        cout<<"Scrivi il valore dell'esponente"<<endl;
+        cin>>y;
+        cont=y;
+        while(cont<=y)
+        {
+            cont++;
+            x=x*x;
+        }
+        cout<<x<<endl;
+        
+
+system("pause");
+
+
+    return 0;
+}
+
